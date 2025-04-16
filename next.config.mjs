653a41f10ff089b236753ec91c17d1a2e1802f03
John <john.tan-aristy@csedge.org>
2025-04-15 23:44:34 -0500

    I swear I am a good developer this only happens sometimes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-import { execSync } from "child_process";
 const nextConfig = {
   generateBuildId: async () => {
     // This could be anything, using the latest git hash
-    return execSync("git rev-parse HEAD").toString().trim();;
+    return process.env.GIT_HASH;
   },
 };
 
