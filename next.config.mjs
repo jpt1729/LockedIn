0abbe049ea43dfb101855232dafdd283ae8d766d
John <john.tan-aristy@csedge.org>
2025-04-15 23:37:37 -0500

    the fixes were stupid ideas tbh

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-import fs from 'fs'
-import path from 'path'
-
-
 const nextConfig = {
   generateBuildId: async () => {
     // This could be anything, using the latest git hash
-    const buildId = fs.readFileSync(path.join(__dirname, "../.next/BUILD_ID"), "utf8");
-    return buildId
+    return process.env.GIT_HASH;
   },
 };
 
--- scripts/inject-build-id.js
+++ scripts/inject-build-id.js
@@ -1,2 +1,7 @@
 // scripts/inject-build-id.js
+const fs = require("fs");
+const path = require("path");
 
+const buildId = fs.readFileSync(path.join(__dirname, "../.next/BUILD_ID"), "utf8");
+fs.writeFileSync(".env.local", `NEXT_PUBLIC_BUILD_ID=${buildId}`);
+console.log("Build ID injected:", buildId);
