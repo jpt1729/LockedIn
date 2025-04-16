5e4945088dbc298d876dcd9ab50d576f113f81bf
John <john.tan-aristy@csedge.org>
2025-04-15 23:43:06 -0500

    maybe this fixes build id

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { execSync } from "child_process";
 const nextConfig = {
   generateBuildId: async () => {
     // This could be anything, using the latest git hash
-    return process.env.GIT_HASH;
+    return execSync("git rev-parse HEAD").toString().trim();;
   },
 };
 
